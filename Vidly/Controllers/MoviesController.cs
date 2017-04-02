41244ddf500b34779e4eba9d146b2ef3f53a6caa
Yongzhi Li <yongli@microsoft.com>
2017-04-02 18:54:47 +0800

    exercise4 done.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -25,7 +27,7 @@ namespace Vidly.Controllers
         {
             var movies = _context.Movies.Include(m => m.Genre).ToList();
 
-            return View(movies);    
+            return View(movies);
         }
 
         public ActionResult Details(int id)
@@ -57,6 +59,51 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public ActionResult New()
+        {
+            ViewBag.Genres = _context.Genres.ToList();
+            return View("MovieForm", new Movie());
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(x => x.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Genres = _context.Genres.ToList();
+            return View("MovieForm", movie);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var dbMovie = _context.Movies.Single(x => x.Id == movie.Id);
+                dbMovie.Name = movie.Name;
+                dbMovie.Genre = movie.Genre;
+                dbMovie.NumberInStock = movie.NumberInStock;
+                dbMovie.ReleaseDate = movie.ReleaseDate;
+                dbMovie.DateAdded = DateTime.Now;
+            }
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                Console.WriteLine(e);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
